72d0f938f2a0a71151ccc0e2b0a45ff5a2290102
manoj kumar gupta <mg8428517@gmail.com>
2026-04-26 18:59:25 +0530

    Added Book Allocation problem solution

---
+++ binarysearch/bookAllocoation.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+bool Ispossible(int arr[], int n, int m, int mid)
+{
+    int studentcount = 1;
+    int pagesum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if(pagesum + arr[i] <= mid){
+            pagesum += arr[i];
+        }
+        else{
+            studentcount++;
+            if(studentcount > m){
+                return false;
+            }
+            pagesum = arr[i];
+        }
+     }
+    return true;
+}
+int bookallocate(int arr[], int n, int m)
+{
+    int s=0;
+    int sum=0;
+    for(int i =0;i<n;i++){
+        sum += arr[i];
+    }
+    int e= sum;
+    int mid = (s+e)/2;
+    while(s<=e){
+        if(Ispossible(arr,n,m,mid)){
+            e = mid -1;
+        }
+        else{
+            s = mid + 1;
+        }
+        mid = (s+e)/2;
+    }   
+    return s;
+}
+
+int main()
+{
+    int arr[] = {10, 20, 30, 40};
+    int n = 4;
+    cout << " book allocate is :" << bookallocate(arr, n, 2);
+}
